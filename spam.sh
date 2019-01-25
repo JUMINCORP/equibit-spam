7edb73c6b6a4b453c653d60474f602c8b640651d
Steve Darveau <steve@darvs.net>
2019-01-25 09:21:03 -0500

    - No longer using specific user for spam node

--- spam.sh
+++ spam.sh
@@ -5,11 +5,6 @@ NAME=spam
 [ -z "$(docker ps -a -q -f name=${NAME})" ] || docker rm ${NAME}
 
 docker run -d --name "${NAME}" \
-	-p 17333:17331 \
-	-p 18333:18331 \
-	-v $(pwd)/data:/data \
-	-e PUID=$(id -u) \
-	-e PGID=$(id -g) \
 	-v /etc/localtime:/etc/localtime \
         -v /etc/timezone:/etc/timezone \
 	darvs/eqb-spam:v01.024

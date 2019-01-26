0dce3b88cd132dcfdae8ea131f680ca72743800c
Steve Darveau <steve@darvs.net>
2019-01-26 13:06:48 -0500

    - Do not use a private key by default

--- spam.sh
+++ spam.sh
@@ -7,11 +7,15 @@ PRIVKEY=cQazFLhD5jXVAkYad2A5yfnPS8G7WPNkV2CDyPxHkcdGPexrpC4U
 [ -z "$(docker ps -q -f name=${NAME})" ] || docker stop ${NAME}
 [ -z "$(docker ps -a -q -f name=${NAME})" ] || docker rm ${NAME}
 
+# -e PRIVKEY=${PRIVKEY} \
+
 docker run -d --name "${NAME}" \
 	-v /etc/localtime:/etc/localtime \
         -v /etc/timezone:/etc/timezone \
 	-v $(pwd)/testnet-alpha/contrib/eqb-spam:/spam \
-	-e PRIVKEY=${PRIVKEY} \
+	-e PUID=$(id -u) \
+	-e PGID=$(id -g) \
+	-v $(pwd)/data2:/data2 \
 	darvs/eqb-spam:v01.024
 
 docker exec -it spam bash
